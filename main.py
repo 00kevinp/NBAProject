d512f219c6e1e5fe1940ff07b850aaf492802d7e
KEVIN PICKELMAN <kp7135@engr.ship.edu>
2025-09-28 12:00:13 -0400

    integrated missing games in order by game_id, this ensures the games are followed as per the original NBA schedule. can now begin working on the data and running some analysis/feature engineering

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
 import re
 from time import sleep
 
+import numpy as np
+from nba_api.stats.endpoints import boxscoretraditionalv2
+
 from NBAProject import finding_data as fd
 import os
 import pandas as pd
 
 def main():
-    # root = tk.Tk()
-    # root.title("NBA Statistics Search")
-    # mainWindow = ttk.Frame(root, padding='10 10 10 10')
-    # mainWindow.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-    #
-    # searchLabel = ttk.Label(mainWindow, text="Enter a current or former NBA player's name...")
-    # searchLabel.grid(column=1, row=1, sticky=tk.W)
 
 
-
-    df = pd.read_csv("2012-24reg_season_statsv2.csv")
-
-    x1 = set()
-    missing_vals = set()
-    for g_id in fd.flattendf():
-        x1.add(int((re.sub('^0+','',g_id))))
-
-    x2 = set()
-    for gg_id in df['GAME_ID']:
-        x2.add(gg_id)
-
-    # print(f"x1: {x1}")
-    # print(f"x2: {x2}")
+    all_stats = pd.read_csv("2012-24reg_season_statsv2.csv")
 
 
-    missing_vals = (x1-x2)
+    missing_stats = pd.read_csv("missing_game_stats.csv")
+    # print(missing_stats)
 
 
+    combined_df = pd.concat([all_stats, missing_stats])
+    combined_df = combined_df.sort_values(by=['GAME_ID']).reset_index(drop=True)
 
-
-
-
-
+    final_file = combined_df.to_csv("final_stats_no_covid.csv")
 
 if __name__ == '__main__':
     main()
